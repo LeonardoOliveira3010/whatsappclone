e64367e3cc1773e47e942bf513dd70ed2b24e2d7
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-09-22 13:08:02 -0300

    Ajustando o arvio App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SideBar from './components/SideBar/SideBar';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <div className="app-window">
+        
+        <SideBar>
+          
+        </ SideBar>
+
+        <div className="contentarea">
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
