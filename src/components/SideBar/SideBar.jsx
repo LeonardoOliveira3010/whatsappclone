b1fd34528fb447c9edcd1162a0c97451d4c1e6ed
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-09-23 09:29:40 -0300

    Criando o componente Sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,12 @@
-import './sidebar.css'
+import React, {useState, useEffect} from 'react';
+import ChatListItem from '../ChatListItem/ChatListItem'
 import Header from '../Header/Header';
 import SearchIcon from '@material-ui/icons/Search';
+import './sidebar.css'
 
 const SideBar = () =>{
+    const[chatlist, setChatList] = useState([{}, {}, {}, {}, {}])
+
     return(
         <>
             <div className="sidebar">
@@ -10,13 +14,14 @@ const SideBar = () =>{
 
                 <div className="search">
                     <div className="search-input">
-                        <SearchIcon fontSise="small" />
+                        <SearchIcon fontSise="small" style={{color:"#919191"}} />
                         <input type="search" placeholder="Procurar ou começar uma conversa" />
                     </div>
                 </div>
 
                 <div className="chatlist">
-            
+                    {/* loop utilizando o map */}
+                    {chatlist.map(() => <ChatListItem key={key}/>)}
                 </div>
             </div>
         </>
